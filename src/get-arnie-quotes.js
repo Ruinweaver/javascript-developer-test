c00ce428f6b7d1e5bd2bb5a5e9e74fff02507449
Terry Roberts <trobert10@hotmail.com>
2025-05-31 17:28:11 +1000

    TR-001 Implemented a fully tested  speedy function to retrieve Arnie Quotes

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,32 @@
 const { httpGet } = require('./mock-http-interface');
 
+/**
+ * Retrieve our favourite Arnie quotes 
+ * @param {*} urls Array of urls to the Arnie Quote Rest API 
+ * @returns Promise of all found Arnie Quotes 
+ */
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+
+  const arnieQuoteApiPromises = urls.map((url) => httpGet(url));
+
+  return Promise.all(arnieQuoteApiPromises).then((responses) => {
+    const arnieQuotes = [];
+    for (const response of responses) {
+
+      //Cater for malformed response objects from the server 
+      const responseBody = response.body ?? null;
+      const message = JSON.parse(responseBody)?.message ?? 'NOT FOUND'
+      let category = 'Arnie Quote';
+
+      if (response?.status !== 200) {
+        category = 'FAILURE';
+      }
+
+      arnieQuotes.push({ [category]: message })
+    }
+    return arnieQuotes;
+  });
+
 };
 
 module.exports = {
